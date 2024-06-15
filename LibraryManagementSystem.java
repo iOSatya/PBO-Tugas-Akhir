a04d9fd9d7bd370fadc68e0f0a24afc6d7963610
Bahiskara A <108352285+kyrozepto@users.noreply.github.com>
2024-06-15 20:00:51 +0700

    Update LibraryManagementSystem.java
    redirect to login

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -1,14 +1,11 @@
 import javax.swing.SwingUtilities;
 
-// Kelas utama untuk menjalankan aplikasi
 public class LibraryManagementSystem {
     public static void main(String[] args) {
-        // Pastikan ini dijalankan di event dispatch thread untuk Swing
-        SwingUtilities.invokeLater(new Runnable() {
+                SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                // Membuat dan menampilkan jendela utama aplikasi
-                new LibraryFrame();
+                                new LoginFrame();
             }
         });
     }
